c6b43616d9a582b8fc8f96b7962a4caeb4fcffa0
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2019-04-16 19:52:07 +0300

    feat: review of app ext scripts

---
+++ src/boot/qmodeltd.js
@@ -0,0 +1,10 @@
+import {
+  QEditableTd, QSelectableTd
+} from 'quasar-app-extension-qmodeltd/src/component'
+
+export default ({
+  Vue
+}) => {
+  Vue.component('QEditableTd', QEditableTd)
+  Vue.component('QSelectableTd', QSelectableTd)
+}
--- src/index.js
+++ src/index.js
@@ -5,59 +5,21 @@
  * API: https://github.com/quasarframework/quasar/blob/master/app/lib/app-extension/IndexAPI.js
  */
 
-const extendWithModelTd = function (api, conf) {
-  // make sure there is a framework section
-  if (!conf.framework) {
-    conf.framework = {}
-  }
-
-  // make sure there is a components array
-  if (!conf.framework.components) {
-    conf.framework.components = []
-  }
-
-  // for brevity
-  let components = conf.framework.components
-
-  // make sure component QTd is added
-  if (!components.includes('QTd')) {
-    console.log(` App Extension (qmodeltd) Info: 'Adding QTd component - consider adding this to your quasar.conf.js'`)
-    components.push('QTd')
-  }
-  // make sure component QPopupEdit is added
-  if (!components.includes('QPopupEdit')) {
-    console.log(` App Extension (qmodeltd) Info: 'Adding QPopupEdit component - consider adding this to your quasar.conf.js'`)
-    components.push('QPopupEdit')
-  }
-  // make sure component QInput is added
-  if (!components.includes('QInput')) {
-    console.log(` App Extension (qmodeltd) Info: 'Adding QInput component - consider adding this to your quasar.conf.js'`)
-    components.push('QInput')
-  }
-
-  // make sure there is a boot array
-  if (!conf.boot) {
-    conf.boot = []
-  }
-
-  // for brevity
-  let boot = conf.boot
-
+const extendWithModelTd = function (conf) {
   // make sure qmodeltd boot file is registered
+  boot.push('~quasar-app-extension-qmodeltd/boot/qmodeltd.js')
 
-  if (!boot.includes('~quasar-app-extension-qmodeltd/boot/qmodeltd.js')) {
-    boot.push('~quasar-app-extension-qmodeltd/boot/qmodeltd.js')
-    // make sure boot file transpiles
-    conf.build.transpileDependencies.push(/quasar-app-extension-qmodeltd[\\/]src[\\/]boot/)
-    console.log(` App Extension (qmodeltdr) Info: 'Adding qmodeltd boot reference to your quasar.conf.js'`)
-  }
+  // make sure boot file transpiles
+  conf.build.transpileDependencies.push(/quasar-app-extension-qmodeltd[\\/]src/)
+  console.log(` App Extension (qmodeltdr) Info: 'Adding qmodeltd boot reference to your quasar.conf.js'`)
 }
 
-module.exports = function (api, ctx) {
-  api.registerDescribeApi('QModelTd', '../component/QModelTd.json')
-  api.registerDescribeApi('QEditableTd', '../component/QEditableTd.json')
-  api.registerDescribeApi('QSelectableTd', '../component/QSelectableTd.json')
-  api.extendQuasarConf((conf) => {
-    extendWithModelTd(api, conf)
-  })
+module.exports = function (api) {
+  api.compatibleWith('@quasar/app', '^1.0.0-beta.18')
+
+  api.registerDescribeApi('QModelTd', './component/QModelTd.json')
+  api.registerDescribeApi('QEditableTd', './component/QEditableTd.json')
+  api.registerDescribeApi('QSelectableTd', './component/QSelectableTd.json')
+
+  api.extendQuasarConf(extendWithModelTd)
 }
